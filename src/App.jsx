403427179455316952fce967bd8f13734c68b111
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-05 19:40:26 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,17 @@ export function App() {
   return (
     <div>
       <h1>Hello world</h1>
-      <Post/>
+      <Post 
+      author="Rebeca Caroba" 
+      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime est numquam, mollitia voluptates tempora ea ipsum recusandae, laudantium alias accusantium dolorem. Architecto dolor tenetur facere possimus quasi suscipit voluptates similique!"
+      />
+      <Post
+      author="Bill Gates"
+      content="O dia tá lindo"
+      />
+
     </div>
   )
 }
+
 
--- src/post.jsx
+++ src/post.jsx
@@ -1,3 +1,8 @@
-export function Post(){
-    return <p>POST</p>
+export function Post(props){
+    return (
+    <div>
+        <strong>{props.author}</strong>
+        <p>{props.content}</p>
+    </div>
+    )
 }
