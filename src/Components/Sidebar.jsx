04cc6591042f1e8371bfb6e881f1f60e0f445475
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-06 13:10:12 -0300

    Finalizando Sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import {PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -7,11 +8,13 @@ export function Sidebar() {
                 src="https://images.pexels.com/photos/394377/pexels-photo-394377.jpeg?auto=compress&cs=tinysrgb&w=500&h=750&dpr=1"
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/rebecacaroba.png" alt="" />
                 <strong>Rebeca Caroba</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
