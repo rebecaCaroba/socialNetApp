ffd6ef1a1f9b51cab2c33f3b2059ee70b1b95ce4
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-07 08:08:19 -0300

    Estilizando Post

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -5,21 +5,26 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img src="https://github.com/rebecacaroba.png" />
+                    <img className={styles.avatar} src="https://github.com/rebecacaroba.png" />
                     <div className={styles.authorInfo}>
                         <strong>Rebeca Caroba</strong>
                         <span>Web Developer</span>
                     </div>
                 </div>
+                <time title="11 de Maio às 16h30" dateTime="2022-08-06 16:30:50">Publicado há 1h</time>
             </header>
-            <time title="11 de Maio às 16h30" dateTime="2022-08-06 16:30:50">Publicado há 1h</time>
+
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
                 <p>    
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex cupiditate inventore explicabo incidunt! Temporibus fugit maxime deleniti facere quidem.
                 </p>
-                <p>👉<a href="">jane.design/doctorcare</a></p>
-                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                <p><a href="">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>
+                    <a href="#">#nlw</a>
+                    <a href="#">#rocketseat</a>
+                </p>    
             </div>
         </article>
     )
