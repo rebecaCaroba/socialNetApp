8b56c9201d4d95d635f4b185bf77e8933bd2197d
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-07 10:02:43 -0300

    Componente Avatar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import {PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -8,7 +9,7 @@ export function Sidebar() {
                 src="https://images.pexels.com/photos/394377/pexels-photo-394377.jpeg?auto=compress&cs=tinysrgb&w=500&h=750&dpr=1"
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/rebecacaroba.png" alt="" />
+                <Avatar hasBorder src="https://github.com/rebecacaroba.png" />
                 <strong>Rebeca Caroba</strong>
                 <span>Web Developer</span>
             </div>
