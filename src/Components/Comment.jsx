2c8e7bb6687642ccb9536939e4d1d1b0d4b5d8b6
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-07 09:28:09 -0300

    Estilizando o Comment

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -11,17 +11,16 @@ export function Comment() {
                             <strong>Rebeca Caroba</strong>
                             <time title='11 de Maio às 08:30h' dateTime='20023-08-07 08:30:45'>Cerca de 1h atrás</time>
                         </div>
-
                         <button title='Deletar comentário'>
                             <Trash size={20} />
                         </button>
                     </header>
-                    <p>Muito vom Steven, parabéns!!</p>
+                    <p>Muito bom, parabéns!!</p>
                 </div>
 
                 <footer>
-                    <ThumbsUp />
-                    Aplaudir <span>20</span>
+                    
+                    <button><ThumbsUp /> Aplaudir <span>20</span></button>
                 </footer>
             </div>
         </div>
